ee27560ba955ef9c21d590ae63240eaeebed5a32
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-03-11 18:16:52 +0500

    Fix(api): use constant over hard code

--- apps/api/src/start.ts
+++ apps/api/src/start.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express, { Application } from "express";
+import { port } from "./lib/constants";
 
 const app: Application = express();
 
-app.listen(5000, () => console.log(`Server started on port 5000`));
+app.listen(port, () => console.log(`Server started on port 5000`));
