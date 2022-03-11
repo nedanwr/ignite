09843249167daf0d8bbb6cb6269df6e416bfc496
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-03-11 18:19:22 +0500

    Feat(api): use morgan if in dev mode

--- apps/api/src/start.ts
+++ apps/api/src/start.ts
@@ -1,7 +1,11 @@
 import "dotenv/config";
 import express, { Application } from "express";
-import { port } from "./lib/constants";
+import { __prod__, port } from "./lib/constants";
 
 const app: Application = express();
+
+if (!__prod__) {
+    app.use(require("morgan")("dev"));
+}
 
 app.listen(port, () => console.log(`Server started on port 5000`));
