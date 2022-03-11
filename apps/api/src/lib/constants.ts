315d193448cd059932f4475083008770ad1414dc
Naveed <46922100+nedanwr@users.noreply.github.com>
2022-03-11 18:21:32 +0500

    Feat(api): create env constant

--- apps/api/src/lib/constants.ts
+++ apps/api/src/lib/constants.ts
@@ -1,2 +1,3 @@
 export const port:string|number = process.env.PORT || 5500;
 export const __prod__:boolean = process.env.NODE_ENV === 'production';
+export const env = process.env.NODE_ENV || 'development';
